619d8e99ab5084dd4be2f9d0bc5ebdcba5c1b1de
Maceluch <celcjusz@gmail.com>
2017-04-04 17:05:02 +0200

    first example
    tabliczka mnożenia

---
+++ main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        List lista = new List();
+
+        System.out.print(lista.size());
+
+        /*for (int i = 0; i < 11; i++) {
+            String s=(i==0) ? "  " : i + " ";
+            System.out.print(s) ;
+        }
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i+1 + " ");
+            for (int j = 0; j < 10; j++) {
+                System.out.print((j+1 )* (i+1));
+                if(j!=9)
+                    System.out.print(";");
+            }
+            System.out.println();
+        }*/
+    }
+}
